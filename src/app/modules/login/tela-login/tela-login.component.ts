9d04df87caa0a274b9d6b9d9e44b0a39a892f06c
Felipe Fontenele <felipefontenele1317@gmail.com>
2022-04-09 16:14:45 -0300

    Adiciona a tela de login ao root

--- src/app/modules/login/tela-login/tela-login.component.ts
+++ src/app/modules/login/tela-login/tela-login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tela-login',
   templateUrl: './tela-login.component.html',
-  styleUrls: ['./tela-login.component.scss']
+  styleUrls: ['./tela-login.component.scss'],
 })
 export class TelaLoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    nomeUsuario: new FormControl('', Validators.required),
+    senhaUsuario: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.loginForm.value);
   }
-
 }
