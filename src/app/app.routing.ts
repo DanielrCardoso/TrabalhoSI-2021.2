7c5ce2f674337ed9bfbb6ceb7fe98c2d44858a30
Felipe Fontenele <felipefontenele1317@gmail.com>
2022-04-10 19:26:06 -0300

    Adiciona a tela de criação de perguntas do form às rotas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,5 @@
-import { ModuleWithProviders } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { TelaLoginComponent } from './modules/login/tela-login/tela-login.component';
 import { TelaInicialComponent } from './modules/inicio/tela-inicial/tela-inicial.component';
@@ -8,15 +8,18 @@ import { TelaPerfilComponent } from './modules/perfil/tela-perfil/tela-perfil.co
 import { TelaDicasComponent } from './modules/dicas/tela-dicas/tela-dicas.component';
 import { TelaForumComponent } from './modules/forum/tela-forum/tela-forum.component';
 import { TelaAulasComponent } from './modules/cursos/tela-aulas/tela-aulas.component';
+import { TelaForumCriarPerguntaComponent } from './modules/forum/tela-forum-criar-pergunta/tela-forum-criar-pergunta.component';
 
 const APP_ROUTES: Routes = [
-    { path: '', component: TelaInicialComponent },
-    { path: 'login', component: TelaLoginComponent },
-    { path: 'cadastro', component: TelaCadastroComponent },
-    { path: 'perfil', component: TelaPerfilComponent },
-    { path: 'dicas', component: TelaDicasComponent },
-    { path: 'forum', component: TelaForumComponent },
-    { path: "aulas", component: TelaAulasComponent }
+  { path: '', component: TelaInicialComponent },
+  { path: 'login', component: TelaLoginComponent },
+  { path: 'cadastro', component: TelaCadastroComponent },
+  { path: 'perfil', component: TelaPerfilComponent },
+  { path: 'dicas', component: TelaDicasComponent },
+  { path: 'forum', component: TelaForumComponent },
+  { path: 'aulas', component: TelaAulasComponent },
+  { path: 'criar-pergunta', component: TelaForumCriarPerguntaComponent },
 ];
 
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
+export const routing: ModuleWithProviders<any> =
+  RouterModule.forRoot(APP_ROUTES);
